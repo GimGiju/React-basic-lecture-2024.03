8c799e13552eadd1bf54d459f51710a8f17d4e14
GimGiju <tigerwonder@naver.com>
2024-03-29 10:20:03 +0900

    3일차 수업

--- src/components/User2.jsx
+++ src/components/User2.jsx
@@ -10,16 +10,16 @@ export default function User() {
   const [form, setForm] = useState({id:'', name:'', email:''});
   const handleSubmit = (event) => {
     event.preventDefault();   // submit button을 누르면 페이지가 자동적으로 바뀌는 것을 방지
-    const existUser = users.find(user => user.id == form.id);
-    const newUsers = [];
+    const existUser = users.find(user => user.id === form.id);
+    // const newUsers = [];
     if (existUser) {
-      setUsers(users.map(user => (user.id == form.id) ? form : user))
-      for (let user of users)
-        if (user.id == form.id)
-          newUsers.push(form);
-        else
-          newUsers.push(user);
-      setUsers(newUsers);
+      setUsers(users.map(user => (user.id === form.id) ? form : user))
+      // for (let user of users)
+      //   if (user.id == form.id)
+      //     newUsers.push(form);
+      //   else
+      //     newUsers.push(user);
+      // setUsers(newUsers);
     } else
       setUsers([...users, form]);
   }
@@ -61,7 +61,7 @@ export default function User() {
       </form>
       <button onClick={() => {
         const id = prompt('Id 값을 입력하세요');
-        setUsers(users.filter(user => user.id != id));
+        setUsers(users.filter(user => user.id !== id));
       }}>삭제</button>
     </div>
   );
