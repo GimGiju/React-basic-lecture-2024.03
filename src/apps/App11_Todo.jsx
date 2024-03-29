a556f85557b30c25495ea25f93e7ff2c3474b65c
GimGiju <tigerwonder@naver.com>
2024-03-29 12:43:21 +0900

    React 2일차 수업

---
+++ src/apps/App11_Todo.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from '../todo_components/Header';
+import TodoList from '../todo_components/TodoList';
+import { useState } from 'react';
+
+function App() {
+  const filters = ['all', 'active', 'completed'];
+  const [filter, setFilter] = useState('all');
+  return (
+    <div className='card'>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </div>
+  );
+}
+
+export default App;
