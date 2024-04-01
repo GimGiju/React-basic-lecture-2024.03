34ff55f31f633d258dbb51a349b2d4cab2c6135d
GimGiju <tigerwonder@naver.com>
2024-04-01 10:20:23 +0900

    리렉트 라우팅 수업

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
-export default function NotFound() {
-  return (
-    <div style={{margin: '20px'}}>
-      Page Not Found Error
-    </div>
-  )
+export default function Home(){
+    return(
+        <div style={{margin: '20px'}}>
+            Home page
+        </div>
+    )
 }
